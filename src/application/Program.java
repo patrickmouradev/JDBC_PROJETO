79e7ae4d13ea432bec17813f0a4bb78b81c22268
Patrick Moura <patrickmoura@gmail.com>
2019-11-12 16:42:46 -0300

    Criando a Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args) {
 	
 	Department obj = new Department(1, "Teste");
-
-	System.out.println(obj);
+	Seller seller = new Seller(21,"BOB","patrickmoura@gmail.com", new Date(), 1000.00, obj);
+	System.out.println(seller);
 	
 	}
 
