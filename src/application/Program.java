a6e65a556842ec655134dbf7333295cc16e0878d
Patrick Moura <patrickmoura@gmail.com>
2019-11-12 22:45:00 -0300

    Criando o Metodo findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,22 +8,27 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
-	
-	public static void main(String[] args) {
-	
-	SellerDao sellerDao = DaoFactory.createSellerDao();
 
-	System.out.println("===Teste Seller Find By Id: ");
-	Seller seller = sellerDao.findById(3);
-	System.out.println(seller);
-	
-	System.out.println("\n ===Teste Seller findByDepartment: ");
-	Department department = new Department(2, null);
-	List<Seller> sellerList = sellerDao.findByDepartment(department);
-	for (Seller objseller : sellerList) {
-		System.out.println(objseller);
-	}
-	
+	public static void main(String[] args) {
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		System.out.println("===Teste Seller Find By Id: ");
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
+
+		System.out.println("\n ===Teste Seller findByDepartment: ");
+		Department department = new Department(2, null);
+		List<Seller> sellerList = sellerDao.findByDepartment(department);
+		for (Seller objseller : sellerList) {
+			System.out.println(objseller);
+		}
+
+		System.out.println("\n ===Teste Seller findAll: ");
+		sellerList = sellerDao.findAll();
+		for (Seller objseller : sellerList) {
+			System.out.println(objseller);
+		}
 	}
 
 }
